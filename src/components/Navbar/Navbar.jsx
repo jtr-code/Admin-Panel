a3d23a9ceb0034f8b031b1eb8ea6c8b523966e98
jtr-code <jtrtricks@gmail.com>
2023-08-10 14:56:47 +0530

    [added] navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,57 @@
 import "./Navbar.scss";
-
+import {
+  Search,
+  Language,
+  DarkModeOutlined,
+  ChatBubbleOutlineOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ListOutlined
+} from "@mui/icons-material";
 const Navbar = () => {
-  return <div>nav</div>;
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <Search />
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <Language className='icon' />
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlined className='icon' />
+          </div>
+          <div className='item'>
+            <FullscreenExitOutlined className='icon' />
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlined className='icon' />
+            <div className='counter'>1</div>
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineOutlined className='icon' />
+            <div className='counter'>2</div>
+          </div>
+          <div className='item'>
+            <NotificationsNoneOutlined className='icon' />
+          </div>
+          <div className='item'>
+            <ListOutlined className='icon' />
+          </div>
+          <div className='item'>
+            <img
+              src='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_960_720.png'
+              alt='avatar'
+              className='avatar'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
